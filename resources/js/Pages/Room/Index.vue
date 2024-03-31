0be0be9097dcf3493f5eb67879dd7d491b29b344
0smarLP <olopezi1400@alumno.ipn.mx>
2024-03-30 20:53:51 -0600

    Actualizar componente de habitaciones en la página principal

--- resources/js/Pages/Room/Index.vue
+++ resources/js/Pages/Room/Index.vue
@@ -1,6 +1,12 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Rooms from "@/Components/Rooms.vue";
+
+const props = defineProps({
+  data: {
+    type: Object,
+  },
+});
 </script>
 
 <template>
@@ -14,7 +20,7 @@ import Rooms from "@/Components/Rooms.vue";
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-          <Rooms />
+          <Rooms :data="data" />
         </div>
       </div>
     </div>
