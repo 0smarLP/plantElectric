aba382a8cf05bd7649e2e9f5a8ae6df0211629b3
0smarLP <olopezi1400@alumno.ipn.mx>
2024-03-30 20:53:21 -0600

    Actualizar importaciones de DataTable en app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,11 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import { DataTable } from 'datatables.net-vue3';
-import { DataTablesLib } from 'datatables.net';
+import DataTable from 'datatables.net-vue3'
+import DataTablesLib from 'datatables.net';
 
 DataTable.use(DataTablesLib);
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
